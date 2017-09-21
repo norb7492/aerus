c8e351b067632c19c64cd2bf31b49bdf0f0cc00e
Norbert Hermanno Strzybny <norbert@brq.com>
2017-09-21 17:01:43 -0300

    login

--- aerus-front/src/app.js
+++ aerus-front/src/app.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Login from './files/login/login';
 
+import 'font-awesome/css/font-awesome.css';
+import 'primereact/resources/themes/omega/theme.css';
+import './primefaces/primereact.scss';
 
+/* import 'primereact/resources/primereact.min.css'; */
+//minified css
 
 ReactDOM.render(<Login/>, document.getElementById('root'));
--- aerus-front/src/files/login/login.js
+++ aerus-front/src/files/login/login.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
 import './login.scss';
 
-import {Button} from 'primereact/components/button/Button';
-import 'primereact/resources/themes/omega/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'font-awesome/css/font-awesome.css';
+import {Dropdown} from 'primereact/components/dropdown/Dropdown';
 
 export default class Login extends Component {
+
+languages = [
+    {label: 'English', value: 'eng-en'},
+    {label: 'Deutsch', value: 'de'},
+    {label: 'Português', value: 'pt'},
+    {label: '日本語', value: 'jp'},
+    
+];
+
   render() {
     return (
       <div className="Login">
-        <h1>Login</h1>
-        <Button />
+          <div className="flexContainer">
+            <div className="loginBox">
+              <div className="loginMenu">
+               <div className="loginTitle">
+                  <p className="loginTitle">Login</p>
+               </div>
+               <div className="dropDown">
+               <Dropdown value='teste' options={this.languages}  style={{width:'150px'}}/>
+               </div>
+               </div> 
+
+            </div>
+        </div> 
       </div>
     );
   }
